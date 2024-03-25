0ae2ff5e522c77449c1fe0d2e5a2d7fa7e92bbc7
Isaias Santos <isaiassousa032@gmail.com>
2024-03-25 07:45:33 -0300

    texto e condicionais

---
+++ condicionais.py
@@ -0,0 +1,16 @@
+# if, elif e else
+
+# Exemplo de "if"
+idade = 18
+print("Exemplo de comando if")
+if idade >= 18:
+    print("Você é maior de idade")
+
+if idade == 19:
+    print("Você tem 19 anos")
+
+if idade <= 18:
+    print("você é menor de idade")
+
+if idade != 10:
+    print("Você não tem 10 anos")
