a5a949bdaa3de6579e99854141919b60002a61f8
Isaias Santos <isaiassousa032@gmail.com>
2024-03-24 21:36:58 -0300

    formatação de texto

---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,8 @@
+nome_completo = "Isaias Sousa dos Santos"
+
+# Formatação de texto
+print("Nome completo (1° forma):", nome_completo)
+print("Nome completo (2° forma): " + nome_completo)
+print(f"Nome completo (3° forma): {nome_completo}")
+print("Nome completo (4° forma): %s" % nome_completo)
+print("Nome completo (5° forma): {}".format(nome_completo))
