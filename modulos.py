147e4cca7dfbc08e455ae581bcabc10afd00757c
Isaias Santos <isaiassousa032@gmail.com>
2024-03-29 15:33:18 -0300

    utilizando modulos

---
+++ modulos.py
@@ -0,0 +1,9 @@
+print("Exemplo de importação de uma módulo padrão:")
+#import math
+from math import sqrt
+
+raiz_quadrada = sqrt(25)
+print(f"A raiz quadrada de 25 é {raiz_quadrada}")
+import meu_modulo
+
+mensagem = meu_modulo.saudacao("Gabriel")
